90efe362118d644718ea3ded841a7934e708fcb6
turzinnnn <152355692+turzinnnn@users.noreply.github.com>
2025-02-26 08:54:26 -0300

    Add files via upload

--- criar_conta_ff.py
+++ criar_conta_ff.py
@@ -1,17 +1,15 @@
-#falta colocar opção no menu de modificar e excluir.
-
 from datetime import datetime
 registros = []
 def ler_valor_nao_vazio(nome_variavel):
     valor_lido = ''
     while valor_lido.strip() == '':
-        print(f"o valor para {nome_variavel} não pode ser vazio!")
-        valor_lido = input(f'Entre com o valor para o(a) {nome_variavel} da conta: ')
+        print(f"\nO valor para {nome_variavel} não pode ser vazio!")
+        valor_lido = input(f'\nEntre com o valor para o(a) {nome_variavel} da conta: ')
 
     return valor_lido
 
 def criar_conta():
-    execs=int(input('Quantos cadastros você irá fazer?:'))
+    execs=int(input('\nQuantos cadastros você irá fazer?:'))
     for i in range(execs):
         nickname = ler_valor_nao_vazio('nickname')
         email = ler_valor_nao_vazio('email')
@@ -28,7 +26,6 @@ def criar_conta():
     print("Conta criada com sucesso!\n")
 
     return conta
-
 def imprimir_conta(conta):
     print(f"Nickname:\t\t{conta['nickname']}")
     print(f"Email:\t\t{conta['email']}")
@@ -44,14 +41,44 @@ def imprimir_todos():
         for conta in registros:
             imprimir_conta(conta)
 
+def excluir_conta():
+    email = input("Digite o email da conta a ser excluída: ")
+    for conta in registros:
+        if conta['email'] == email:
+            registros.remove(conta)
+            print("Conta excluída com sucesso!\n")
+            return
+    print("Conta não encontrada.\n")
+
+
+def modificar_conta():
+    email = input("Digite o email da conta a ser modificada: ")
+    for conta in registros:
+        if conta['email'] == email:
+            print("Deixe os campos em branco se não quiser alterá-los.")
+            novo_nickname = input("Novo nickname: ") or conta['nickname']
+            nova_dataNascimentoString = input("Nova data de nascimento (dd/mm/aaaa): ") or conta[
+                'dataNascimento'].strftime('%d/%m/%Y')
+            nova_dataNascimento = datetime.strptime(nova_dataNascimentoString, "%d/%m/%Y")
+            nova_senha = input("Nova senha: ") or conta['senha']
+
+            conta.update({
+                "nickname": novo_nickname,
+                "dataNascimento": nova_dataNascimento,
+                "senha": nova_senha
+            })
+            print("Conta modificada com sucesso!\n")
+            return
+    print("Conta não encontrada.\n")
+
 def menu():
     while True:
         print("\n******Bem vindo ao Fogaréu Grátis******")
         print("\nMenu:")
         print("1 - Adicionar nova conta")
-        print("2 -  Exibir todas as contas")
-        print("3 - Excluir conta")
-        print("4 - Modificar conta")
+        print("2 - Exibir todas as contas")
+        print("3 - Modificar conta")
+        print("4 - Excluir conta")
         print("5 - Sair")
 
         opcao = input("Escolha uma opção:")
@@ -60,6 +87,10 @@ def menu():
             criar_conta()
         elif opcao == '2':
             imprimir_todos()
+        elif opcao == '3':
+            modificar_conta()
+        elif opcao == '4':
+            excluir_conta()
         elif opcao == '5':
             print("Saindo...")
             break
